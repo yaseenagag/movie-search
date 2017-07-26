e779142efb39fb0ae27fa292a22614443437536a
yaseenagag <yaseen.uc@gmail.com>
2017-07-25 18:06:22 -0700

    create a git request to IMDP and parse the data into an array of strings

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const cheerio = require('cheerio')
+
+function queryIMDP(search) {
+  http.get({
+    host: 'www.imdb.com',
+    path: `/find?ref_=nv_sr_fn&q=${search}&s=all`
+  }, (res) => {
+    var html = ''
+    res.on('data', (chunk) => { html += chunk })
+    res.on('end', () => {
+      parseHTML(html)
+    })
+  })
+}
+
+function parseHTML(html) {
+  const $ = cheerio.load(html)
+  const movieNames = $('.result_text')
+    .map((index, element) => $(element).text())
+    .toArray()
+  console.log('------------------------------------');
+  console.log(movieNames);
+  console.log('------------------------------------');
+}
+
+
+queryIMDP("nemo")
